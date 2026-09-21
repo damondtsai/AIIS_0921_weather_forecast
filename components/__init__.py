224a77a6d58322fb55368b05ab1ccff11ec81b91
damondtsai <damondtsai@users.noreply.github.com>
2026-09-21 20:47:13 +0800

    fix: export render_cwa_hero_summary in components init

--- components/__init__.py
+++ components/__init__.py
@@ -1,1 +1,14 @@
 """Taiwan Weather Forecast UI Components."""
+from components.weather_cards import render_weather_cards, render_cwa_hero_summary, get_wx_icon
+from components.weather_map import render_weather_map
+from components.weather_chart import render_weather_charts
+from components.weather_table import render_weather_table
+
+__all__ = [
+    "render_weather_cards",
+    "render_cwa_hero_summary",
+    "get_wx_icon",
+    "render_weather_map",
+    "render_weather_charts",
+    "render_weather_table",
+]
